a57a634ad1ae90f3355408f313e1a531b65afc6c
rodrigo <rodrigolouzada.dev@gmail.com>
2023-11-13 19:48:54 -0300

    creating a table(livros) using knex

--- knexfile.js
+++ knexfile.js
@@ -1,47 +1,19 @@
-// Update with your config settings.
-
-/**
- * @type { Object.<string, import("knex").Knex.Config> }
- */
 module.exports = {
 
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
+      filename: './src/data/editora.db3'
+    } ,
+  
+    useNullAsDefault : true ,
+    
+    migrations: {
+      directory : "./src/data/migrations",
+    } ,
+
+    seeds : {
+      directory : "./src/data/seeds"
     }
   },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
-};
+}
