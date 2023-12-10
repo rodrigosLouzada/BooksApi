7a6478888449ce63cce0a35c499f389afce4fe0f
Rodrigo Louzada <rodrigolouzada.dev@gmail.com>
2023-12-10 02:36:31 -0300

    Adding PM2 for production.

--- src/app.js
+++ src/app.js
@@ -7,6 +7,19 @@ const livros = require("./livros");
 
 app.use("/livros", livros);
 
+app.get("/home", (request, response) => {
+    response.status(200).json({
+        message: "Hello, you are accessing the '/home' route of my Books API. Everything is okay here! :)",
+        stack: "Node.js, Express, Knex, SQLite",
+        author:  {
+            name: "Rodrigo Louzada",
+            github: "https://github.com/rodrigosLouzada",
+            gitlab: "https://gitlab.com/rodrigosLouzada",
+            linkedin: "https://www.linkedin.com/in/rodrigo-louzada-443b682a0/"
+        }
+    });
+});
+
 app.get("/", (req, res) => {
     res.send("ola mundo")
 });
